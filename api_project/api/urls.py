f152dc0cdf4b7f04df20bfdb99ca200262562e31
i.ariella <isingizweariella@gmail.com>
2025-03-09 21:35:03 +0200

    development

---
+++ api_project/api/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import BookList
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include API routes
+]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# Initialize the router
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('', include(router.urls)),  # Include the router URLs for CRUD operations
+]
+
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    # other URLs...
+]
